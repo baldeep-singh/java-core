0f16aa395270a078bd18d510cd26c8db7cdf0ece
Baldeep Singh <54467135+baldeep-singh@users.noreply.github.com>
2024-03-08 03:58:30 +0530

    Update D03P03.java

--- src/com/learning/core/day3/D03P03.java
+++ src/com/learning/core/day3/D03P03.java
@@ -7,18 +7,16 @@ public class D03P03
         // Initialize the array
         int[] array = {3, 2, 4, 5, 6, 7, 3, 2, 3, 4, 7, 1, 2, 0, 0, 0,0,0};
 
-        // Compute the sum of elements from index 0 to 14 and store it in the 15th index
         int sum = 0;
         for (int i = 0; i < 15; i++) {
             sum += array[i];
         }
         array[15] = sum;
 
-        // Compute the average of all numbers and store it in the 16th index
         double average = (double) sum / 15;
         array[16] = (int) average;
 
-        // Find the smallest number and store it in the 17th index
+       
         int smallest = array[0];
         for (int i = 1; i < 15; i++) {
             if (array[i] < smallest) {
@@ -27,7 +25,7 @@ public class D03P03
         }
         array[17] = smallest;
 
-        // Print the updated array
+    
         System.out.println("Updated Array: " + Arrays.toString(array));
     }
 }
