771cade9119712d23d062350e0ebb40593faae36
Baldeep Singh <54467135+baldeep-singh@users.noreply.github.com>
2024-03-08 01:38:40 +0530

    Update D01P01.java

--- src/com/learning/core/day1/D01P01.java
+++ src/com/learning/core/day1/D01P01.java
@@ -1,33 +1,26 @@
-package day_1;
+package com.learning.core.day1;
 
-import java.util.ArrayList;
+
 
 public class D01P01 {
 
-	public static void createBook(ArrayList<Book> books, String title, double price) {
+	public static void createBook( String title, double price) {
 		Book newBook = new Book();
 		newBook.setTitle(title);
 		newBook.setPrice(price);
-		books.add(newBook);
+
 	}
 
-	public static void showBooks(ArrayList<Book> books) {
-		for (Book book : books) {
+	public static void showBooks() {
+		
 			System.out.println("Title: " + book.getTitle());
 			System.out.println("Price: " + book.getPrice());
-			System.out.println();
-		}
+			
 	}
 
 	public static void main(String[] args) {
-		// Creating an ArrayList to store books
-		ArrayList<Book> books = new ArrayList<>();
-
-		// Adding books
-		createBook(books, "Java Programming", 350.00);
-		createBook(books, "Data Structures", 500.00);
-
-		// Showing books
-		showBooks(books);
+		
+		createBook("Java Programming", 350.00);
+		showBook();
 	}
 }
