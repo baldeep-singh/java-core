311d0ae84a542144930d20dbe7ad008a750cf1f4
Baldeep Singh <54467135+baldeep-singh@users.noreply.github.com>
2024-03-08 03:30:01 +0530

    Update D03P09.java

--- src/com/learning/core/day3/D03P09.java
+++ src/com/learning/core/day3/D03P09.java
@@ -1,67 +1,71 @@
 package com.learning.core.day3;
-import java.util.*;
 
-public class D03P09 {
-    public static void main(String[] args) {
-        String[] words = {"abc", "cd", "def", "gha"};
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        if (canFormCircle(words)) {
-            System.out.println("The given strings can be chained to form a circle.");
-        } else {
-            System.out.println("The given strings cannot be chained to form a circle.");
-        }
+public class D03P09 {public static boolean canFormCircle(ArrayList<String> strings) {
+    boolean[] visited = new boolean[strings.size()];
+  
+    for(int i = 0; i < strings.size(); i++) {
+        visited[i] = false;
     }
+    
 
-    public static boolean canFormCircle(String[] words) {
-        Map<Character, List<String>> graph = buildGraph(words);
-        Set<String> visited = new HashSet<>();
-
-        for (String word : words) {
-            if (dfs(word, word, graph, visited)) {
-                return true;
-            }
+    for (int i = 0; i < strings.size(); i++) {
+        visited[i] = true;
+        if (dfs(strings, visited, strings.get(i), strings.get(i).charAt(0))) {
+            return true;
         }
-
-        return false;
+        visited[i] = false;
     }
+    return false;
+}
 
-    private static Map<Character, List<String>> buildGraph(String[] words) {
-        Map<Character, List<String>> graph = new HashMap<>();
 
-        for (String word : words) {
-            char startChar = word.charAt(0);
-            char endChar = word.charAt(word.length() - 1);
-            if (!graph.containsKey(startChar)) {
-                graph.put(startChar, new ArrayList<>());
-            }
-            graph.get(startChar).add(word);
-            // Initialize nodes that have no outgoing edges
-            if (!graph.containsKey(endChar)) {
-                graph.put(endChar, new ArrayList<>());
-            }
+    public static boolean dfs(ArrayList<String> strings, boolean[] visited, String curr, char firstChar) {
+        if (allVisited(visited)) {
+            return true;
         }
-
-        return graph;
-    }
-
-    private static boolean dfs(String startWord, String currentWord, Map<Character, List<String>> graph, Set<String> visited) {
-        char endChar = currentWord.charAt(currentWord.length() - 1);
-
-        if (!graph.containsKey(endChar)) {
-            return false;
-        }
-
-        List<String> nextWords = graph.get(endChar);
-        for (String nextWord : nextWords) {
-            if (!visited.contains(nextWord)) {
-                visited.add(nextWord);
-                if (startWord.equals(nextWord) || dfs(startWord, nextWord, graph, visited)) {
-                    return true;
+        for (int i = 0; i < strings.size(); i++) {
+            if (!visited[i]) {
+                if (strings.get(i).charAt(0) == curr.charAt(curr.length() - 1)) {
+                    visited[i] = true;
+                    if (dfs(strings, visited, strings.get(i), firstChar)) {
+                        return true;
+                    }
+                    visited[i] = false;
+                    return false;
                 }
-                visited.remove(nextWord);
             }
         }
-
         return false;
+    }
+
+    public static boolean allVisited(boolean[] visited) {
+        for (boolean v : visited) {
+            if (!v) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> strings = new ArrayList<>();
+        System.out.println("Enter strings (press Enter to stop):");
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            strings.add(line);
+        }
+        scanner.close();
+        if (canFormCircle(strings)) {
+            System.out.println("Possible");
+        } else {
+            System.out.println("Not possible.");
+        }
     }
 }
